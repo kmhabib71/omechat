32733ed712aee31023fa4e25bf1c3ce8b2cff31f
km Habib <31393442+kmhabib71@users.noreply.github.com>
2023-04-12 00:09:11 +0600

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const path = require("path");
 const bodyparser = require("body-parser");
 
 const app = express();
-const dotenv = require("dotenv");
 const connectDB = require("./Server/database/connection");
 
-dotenv.config({ path: "config.env" });
+
 const PORT = process.env.PORT || 8080;
 
 connectDB();
