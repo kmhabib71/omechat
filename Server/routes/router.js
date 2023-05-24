2dc75678cdf202c778e2c470c28003a1387f3924
km Habib <31393442+kmhabib71@users.noreply.github.com>
2023-05-24 14:52:34 +0600

    Update router.js

--- Server/routes/router.js
+++ Server/routes/router.js
@@ -11,6 +11,10 @@ route.post("/api/users", controller.create);
 route.put("/leaving-user-update/:id", controller.leavingUserUpdate);
 route.put("/new-user-update/:id", controller.newUserUpdate);
 route.post("/get-remote-users", controller.remoteUserFind);
+route.put(
+  "/update-on-otheruser-closing/:id",
+  controller.updateOnOtherUserClosing
+);
 route.put("/update-on-engagement/:id", controller.updateOnEngagement);
 route.put("/update-on-next/:id", controller.updateOnNext);
 route.post("/get-next-user", controller.getNextUser);
